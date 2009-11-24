99dd5cbb9d4f8b9ce1f6e47baea258137d7901b6
Greg Tangey <greg@digitalignition.net>
2009-11-24 11:17:03 +0800

    Initial 0.0.1 Commit

---
+++ think-of-the-children/readme.txt
@@ -0,0 +1,24 @@
+=== Think of The Children (ToTC) ===
+Contributors: ruxton
+Donate link: http://ignite.digitalignition.net/articlesexamples/think-of-the-children-table-of-contents-plugin/
+Tags: shortcode,table of contents,toc,child pages,children page,child page,pages,page
+Requires at least: 2.8.1
+Tested up to: 2.8.4
+Stable tag: 0.1
+
+Shortcodes for a table of contents based on child pages
+
+== Description ==
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Unzip the contents of the zip file
+2. Upload the `think-of-the-children` directory to the `/wp-content/plugins/` directory on your website
+3. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
---
+++ think-of-the-children/totc.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Plugin Name:	Think of The Children
+Plugin URI:		http://ignite.digitalignition.net/articlesexamples/think-of-the-children-table-of-contents-plugin/
+Description:	Shortcodes for a table of contents based on child pages
+Author:			Greg Tangey
+Author URI:		http://ignite.digitalignition.net/
+Version:		0.0.1
+*/
+
+/*  Copyright 2009  Greg Tangey  (email : greg@digitalignition.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function totc_children( $atts )
+{
+	global $post;
+	global $more;
+	$more = 0;
+	$return = "";
+	
+	if($post->ID)
+	{
+		$args = array (
+			'post_parent' => $post->ID,
+			'post_type' => 'page',
+			'orderby' => 'date',
+			'order' => 'DESC',
+		);
+		$children = get_posts($args);
+
+		if ($children) {
+			foreach ($children as $post) {
+				setup_postdata($post);
+				$more = 0;
+				$content = get_the_content('');
+				$content = apply_filters('the_content', $content);
+				$content = str_replace(']]>', ']]&gt;', $content);
+
+				$return .= '<h3><a href="'.get_permalink().'">'.get_the_title().'</a></h3>
+'.$content;
+			}
+		}
+
+	}
+
+	return $return;
+}
+
+function totc_parent( $atts )
+{
+	global $post;
+	
+	extract( shortcode_atts( array (
+			'text' => 'Return to Parent Article'
+		), $atts ) );
+
+	if($post->post_parent)
+	{
+		$args = array(
+			'ID' => $post->post_parent,
+			'numberposts' => 1
+		);
+
+		$parent = get_posts($args);
+	}
+	else
+	{
+		$return = "";
+	}
+
+}
+
+add_shortcode('totc_children', 'totc_children');
+add_shortcode('totc_parent', 'totc_parent');
+?>
